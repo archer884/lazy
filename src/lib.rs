4039420ab86f9fadcd5bb7bf1c8c240b397ff8a5
J/A archer884@gmail.com <archer884@gmail.com>
2015-02-28 03:38:33 -0600

    Correct doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 //! Lazily initialized values for Rust
-//! 
+//!
 //! This library basically ports the .NET Lazy<T> class straight
 //! into Rust, permitting the use of values that aren't initialized
 //! until (if) they are called for by your code.
-//! 
+//!
 //! # Example
-//! 
+//!
 //! ```
 //! use lazy::Lazy;
-//! 
+//!
 //! let mut lazy_5 = Lazy::new(|| 5);
-//! 
+//!
 //! assert!(lazy_5.value() == &5);
 //! ```
 
@@ -35,10 +35,10 @@ impl<T, F> Lazy<T, F>
         }
     }
 
-    // Returns a reference to the  lazily-initialized
-    // value created by the function used to create
-    // provided to the constructor. I think this is neat
-    // because it's recursive.
+    /// Returns a reference to the  lazily-initialized
+    /// value created by the function used to create
+    /// provided to the constructor. I think this is neat
+    /// because it's recursive.
     pub fn value(&mut self) -> &T {
         match self.t {
             Some(ref t) => &t,
